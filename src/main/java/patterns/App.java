1f0ed35802b7e39553bf504e031a20a66aa984bc
Jakub Rogowski <jakerogowski33@gmail.com>
2022-06-01 13:53:07 +0200

    Code refactor in builder package

--- src/main/java/patterns/App.java
+++ src/main/java/patterns/App.java
@@ -1,7 +1,5 @@
 package patterns;
 
-import patterns.builder.hero.*;
-
 /**
  * Hello world!
  *
@@ -10,20 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        Hero mage = new Hero.Builder("Solmyr", Profession.MAGE)
-                .addHairType(HairType.BALD)
-                .addHairColor(HairColor.BLACK)
-                .addWeapon(Weapon.WARHAMMER)
-                .build();
-        System.out.println(mage.toString());
-
-        Hero warrior = new Hero.Builder("Ragnar", Profession.WARRIOR)
-                .addHairType(HairType.SHORT)
-                .addHairColor(HairColor.BROWN)
-                .addArmor(Armor.CHAIN_MAIL)
-                .addWeapon(Weapon.AXE)
-                .addSkill(Skill.LEADERSHIP)
-                .build();
-        System.out.println(warrior.toString());
+        System.out.println( "This project is to learn about design patterns." );
     }
 }
