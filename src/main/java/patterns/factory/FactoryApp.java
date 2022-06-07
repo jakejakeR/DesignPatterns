58e62338fa7a295978c3e934bdd607bc6ebf9f3a
Jakub Rogowski <jakerogowski33@gmail.com>
2022-06-07 16:15:52 +0200

    CoinFactory test

--- src/main/java/patterns/factory/FactoryApp.java
+++ src/main/java/patterns/factory/FactoryApp.java
@@ -1,9 +1,18 @@
 package patterns.factory;
 
+import patterns.factory.coins.Coin;
+import patterns.factory.coins.CoinFactory;
+import patterns.factory.coins.CoinType;
+
 public class FactoryApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Factory design pattern practice");
+        System.out.println("Factory design pattern practice\r\n");
+
+        Coin coin1 = CoinFactory.getCoin(CoinType.GOLD);
+        Coin coin2 = CoinFactory.getCoin(CoinType.COPPER);
+        System.out.println(coin1.getDescription());
+        System.out.println(coin2.getDescription());
     }
 }
