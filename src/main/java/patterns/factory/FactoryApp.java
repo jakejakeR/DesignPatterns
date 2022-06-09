9caa42f4325114d0c59a1c74a65af64313bc779c
Jakub Rogowski <jakerogowski33@gmail.com>
2022-06-09 13:45:17 +0200

    NotificationFactory test

--- src/main/java/patterns/factory/FactoryApp.java
+++ src/main/java/patterns/factory/FactoryApp.java
@@ -1,11 +1,16 @@
 package patterns.factory;
 
+import patterns.factory.notifications.Notification;
+import patterns.factory.notifications.NotificationFactory;
+
 public class FactoryApp {
 
     public static void main(String[] args) {
 
         System.out.println("Factory design pattern practice\r\n");
 
-        
+        NotificationFactory notificationFactory = new NotificationFactory();
+        Notification notification = notificationFactory.createNotification("SMS");
+        notification.notifyUser();
     }
 }
