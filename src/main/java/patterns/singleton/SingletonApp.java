7c2f91378c9053b263885daf124af6ff11265495
Jakub Rogowski <jakerogowski33@gmail.com>
2022-06-15 14:12:19 +0200

    Added SingletonApp class

---
+++ src/main/java/patterns/singleton/SingletonApp.java
@@ -0,0 +1,9 @@
+package patterns.singleton;
+
+public class SingletonApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Factory design pattern practice\r\n");
+    }
+}
