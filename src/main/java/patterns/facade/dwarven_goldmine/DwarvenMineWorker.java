636f44c8adddae3fb48e966ce557face442dc296
Jakub Rogowski <jakerogowski33gmail.com>
2022-08-01 23:17:19 +0200

    abstract DwarvenMineWorker class added

--- src/main/java/patterns/facade/dwarven_goldmine/DwarvenMineWorker.java
+++ src/main/java/patterns/facade/dwarven_goldmine/DwarvenMineWorker.java
@@ -1,6 +1,54 @@
 package patterns.facade.dwarven_goldmine;
 
-public class DwarvenMineWorker {
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.Arrays;
+
+/**
+ * DwarvenMineWorker is one of the goldmine subsystems.
+ */
+@Slf4j
+public abstract class DwarvenMineWorker {
+
+    public abstract void work();
+    public abstract String name();
+
+
+    public void action(Action... actions) {
+        Arrays.stream(actions).forEach(this::action);
+    }
+
+    private void action(Action action) {
+        switch(action) {
+            case GO_TO_SLEEP -> goToSleep();
+            case WAKE_UP -> wakeUp();
+            case GO_HOME -> goHome();
+            case GO_TO_MINE -> goToMine();
+            case WORK -> work();
+            default -> LOGGER.info("Undefined action");
+        }
+    }
+
+    enum Action {
+        GO_TO_SLEEP, WAKE_UP, GO_HOME, GO_TO_MINE, WORK
+    }
+
+    //region LOGS
+    public void goToSleep() {
+        LOGGER.info("{} goes to sleep.", name());
+    }
+
+    public void wakeUp() {
+        LOGGER.info("{} wakes up.", name());
+    }
+
+    public void goHome() {
+        LOGGER.info("{} goes home.", name());
+    }
+
+    public void goToMine() {
+        LOGGER.info("{} goes to the mine.", name());
+    }
+    //endregion
 }
 
