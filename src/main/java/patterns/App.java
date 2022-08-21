fd3c55e4d6409e492b5a45f8d85f7083d6ad6df2
Jakub Rogowski <jakerogowski33@gmail.com>
2022-08-21 10:15:33 +0200

    removed scope for lombok

--- src/main/java/patterns/App.java
+++ src/main/java/patterns/App.java
@@ -2,12 +2,9 @@ package patterns;
 
 /**
  * This project is to learn about design patterns.
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "This project is to learn about design patterns." );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("This project is to learn about design patterns.");
     }
 }
