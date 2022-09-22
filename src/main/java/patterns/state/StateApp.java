3fcbc19c6c993559789440aa4d53d6abde3bd420
Jakub Rogowski <jakerogowski33gmail.com>
2022-09-22 18:53:10 +0200

    State pattern test

--- src/main/java/patterns/state/StateApp.java
+++ src/main/java/patterns/state/StateApp.java
@@ -1,10 +1,13 @@
 package patterns.state;
 
 import patterns.state.animal_observer.Animal;
+import patterns.state.animal_observer.CalmState;
 
 public class StateApp {
     public static void main(String[] args) {
         Animal animal = new Animal();
+        animal.setState(new CalmState(animal));
+
         for (int i = 0; i < 4; i++) {
             animal.observe();
             animal.onTimePassed();
